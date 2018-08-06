6057626a3facef7e724a4e542df052e0aa987a45
Brandon Coats <bgcoats1313@gmail.com>
2018-08-06 10:49:42 -0600

    new version of please db, please run

--- pleasedb.sql
+++ pleasedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 29, 2018 at 03:00 PM
+-- Generation Time: Aug 06, 2018 at 04:48 PM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -34,7 +34,14 @@ CREATE TABLE IF NOT EXISTS `data` (
   `page` text NOT NULL,
   `content` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`id`, `page`, `content`) VALUES
+(1, 'FrontEnd/Pages/page.html', 'There once was a man from Wisconsin, who thought his name was John, but everyone called him Jack, then there was Brandon, loud and brash, who called him William, Daniel, Johnson, Nash. Brandon is dead now killed by the man, who upon last calling, was know as Fran');
 
 -- --------------------------------------------------------
 
@@ -44,9 +51,25 @@ CREATE TABLE IF NOT EXISTS `data` (
 
 DROP TABLE IF EXISTS `image`;
 CREATE TABLE IF NOT EXISTS `image` (
-  `id` int(11) NOT NULL,
-  `page` int(11) NOT NULL,
-  `path` int(11) NOT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `page` text NOT NULL,
+  `path` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(250) NOT NULL,
+  `password` text NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
