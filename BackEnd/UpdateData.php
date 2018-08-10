3fc5fd6dbbdbc4b028e1b158c5fdce3fd7d8faaa
JakoAlltrades <Jpriemo@gmail.com>
2018-08-09 22:35:38 -0600

    Update actually works :panda_face:
    It was a stupid string format issue... I am sad panda now :droplet:
    :panda_face: :droplet:

--- BackEnd/UpdateData.php
+++ BackEnd/UpdateData.php
@@ -2,10 +2,12 @@
     header("Access-Control-Allow-Origin: *");
     include 'dbconfig.php';
 
-    $update = "Update data set data.page = ".$mysqli->real_escape_string($_GET['page']).", data.content = ".$mysqli->real_escape_string($_GET['content'])." where data.id = ".$mysqli->real_escape_string($_GET['id']).";";
+    $update = 'Update data set page = "'.$mysqli->real_escape_string($_GET['page']).'", content = "'.$mysqli->real_escape_string($_GET['content']).'" where id = '.$mysqli->real_escape_string($_GET['id']).";";
 
+    //echo "{ 'info': ".$update."}"
 $result = $mysqli->exec($update);
 echo $result;
+if($result != null){
 //disconnect from database
 $result->free();
 $mysqli->close();
