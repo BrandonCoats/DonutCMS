32a240a430775c28a820e11401f85b4f905b83f7
Brandon Coats <bgcoats1313@gmail.com>
2018-08-09 19:35:26 -0600

    Fixed the backend to load page from database

--- FrontEnd/Scripts/Edit.js
+++ FrontEnd/Scripts/Edit.js
@@ -1,2 +1,65 @@
 javascript:document.body.contentEditable='true'; 
 document.designMode='on'; void 0
+//The above code creates the illusion of constant updates for the user
+// function setValues()
+// {//actually provides functionality for updating 
+// var contentData = document.getElementById("content").innerHTML;
+// var imageData = document.getElementById("image").innerHTML;
+// console.log(contentData);
+// console.log(imageData);
+// }
+window.onload = function(){
+    LoadPage();
+}
+var data;
+var request = new XMLHttpRequest();
+
+
+function LoadPage() {
+  var page = document.getElementById("title").value;
+  console.log(contentTerms);
+  console.log(page);
+  
+  //var search = 'http://localhost/class/movieAPI/search_results.php' + '?query=' + searchTerms;
+  var search = 'http://localhost/class/DonutCms/BackEnd/LoadPage.php' + '?page=' + page;
+
+  console.log(search);
+  request.open('POST', search);
+  request.onload = loadComplete;
+  request.send();
+}
+
+function loadComplete(evt) {
+  data = JSON.parse(request.responseText);
+  //data = request.responseText;
+  Movies = data.Movies;
+  document.getElementById("res").innerHTML = "Results:";
+  var resultBox = document.getElementById("results");
+  resultBox.innerHTML = "";
+  console.log(Movies);
+  
+  }
+function Save()
+{//Actually makes the callls to save data from fields to database.
+    var data;
+    var request = new XMLHttpRequest();
+    var contentTerms = document.getElementById("content").value;
+    var page = "edit";
+    console.log(contentTerms);
+    console.log(page);
+    
+    //var search = 'http://localhost/class/movieAPI/search_results.php' + '?query=' + searchTerms;
+    var search = 'http://10.10.16.191/back-end/AddData.php' + '?query=' + contentTerms + '&page=' + page;
+  
+    console.log(search);
+    request.open('POST', search);
+    request.onload = loadComplete;
+    request.send();
+    
+}
+// window.setInterval(
+// function()
+// {
+//    setValues();
+// }
+// , 5000);
