1ece89c670f547adc683fbd8b19d4599f3073502
JakoAlltrades <Jpriemo@gmail.com>
2018-08-11 12:51:11 -0600

    Hashing

--- Display/Scripts/Salt&HashBE.js
+++ Display/Scripts/Salt&HashBE.js
@@ -1,7 +1,14 @@
 var bcrypt = require('bcrypt-nodejs');
 
 function comparePassToHash(enteredPass, storedHash){
-    bcrypt.compare(enteredPass, storedHash), function(err, res){
-        return res;
-    }
+    bcrypt.compare(enteredPass, storedHash, function(err, res){
+        if(res)
+        {
+             console.log("Successfully logged in")
+        }
+        else{
+            console.log(err);
+            return err;
+        }
+    });
 }
