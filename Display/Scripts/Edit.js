3b8342c6311495b36fcfea2ea1b44c97fecc76f5
JakoAlltrades <Jpriemo@gmail.com>
2018-08-12 17:31:41 -0600

    Maybe works got rid of the var filename

--- Display/Scripts/Edit.js
+++ Display/Scripts/Edit.js
@@ -26,10 +26,9 @@ function Save()
     imgRequest.send();
 
 }
-function StoreImg()
-{
+
     document.querySelector('input[type="file"]').addEventListener('change', function() {
-        var filename = null;
+        
         if (this.files && this.files[0]) {
             filename = this.files[0];
             var img = document.querySelector('image');  // $('img')[0]
@@ -37,5 +36,3 @@ function StoreImg()
             img.onload = imageIsLoaded; // optional onload event listener
         }
     });
-   return filename;
-}
